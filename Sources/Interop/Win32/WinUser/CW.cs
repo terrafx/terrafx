66cd985cc29f03f9b6828ab16612ebc5e2973257
Tanner Gooding <tagoo@outlook.com>
2017-06-25 15:18:49 -0700

    Adding the 'Color' enum and 'CW.USEDEFAULT' constant to the 'TerraFX.Interop.Win32.WinUser' project.

---
+++ Sources/Interop/Win32/WinUser/CW.cs
@@ -0,0 +1,9 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+namespace TerraFX.Interop
+{
+    public static class CW
+    {
+        public const uint USEDEFAULT = 0x80000000;
+    }
+}
