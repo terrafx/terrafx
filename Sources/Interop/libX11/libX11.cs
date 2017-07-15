36bc89269c3169ee1a7e88013ea53c566604b0d5
Tanner Gooding <tagoo@outlook.com>
2017-07-15 10:17:31 -0700

    Adding various window constants to the libX11 interop types.

--- Sources/Interop/libX11/libX11.cs
+++ Sources/Interop/libX11/libX11.cs
@@ -1,6 +1,6 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
-// Ported from X11\xlib.h in the Xlib - C Language X Interface: X Version 11, Release 7.7
+// Ported from X11\xlib.h and X11\X.h in the Xlib - C Language X Interface: X Version 11, Release 7.7
 // Original source is Copyright © The Open Group.
 
 using System;
@@ -12,16 +12,567 @@ namespace TerraFX.Interop
 {
     unsafe public static partial class libX11
     {
+        #region XID Constants
+        public const uint None = 0;
+        #endregion
+
+        #region Background Pixmap Constants
+        public const int ParentRelative = 1;
+        #endregion
+
+        #region Border Pixmap Constants
+        public const int CopyFromParent = 0;
+        #endregion
+
+        #region SendEvent Destination Window Constants
+        public const int PointerWindow = 0;
+
+        public const int InputFocus = 1;
+        #endregion
+
+        #region Focus Window Constants
+        public const int PointerRoot = 1;
+        #endregion
+
+        #region GetProperty Constants
+        public const int AnyPropertyType = 0;
+        #endregion
+
+        #region GrabKey Constants
+        public const int AnyKey = 0;
+        #endregion
+
+        #region GrabButton Constants
+        public const int AnyButton = 0;
+        #endregion
+
+        #region KillClient Constants
+        public const int AllTemporary = 0;
+        #endregion
+
+        #region Time Constants
+        public const int CurrentTime = 0;
+        #endregion
+
+        #region KeySym Constants
+        public const int NoSymbol = 0;
+        #endregion
+
+        #region Event Mask Constants
+        public const int NoEventMask = 0;
+
+        public const int KeyPressMask = (1 << 0);
+
+        public const int KeyReleaseMask = (1 << 1);
+
+        public const int ButtonPressMask = (1 << 2);
+
+        public const int ButtonReleaseMask = (1 << 3);
+
+        public const int EnterWindowMask = (1 << 4);
+
+        public const int LeaveWindowMask = (1 << 5);
+
+        public const int PointerMotionMask = (1 << 6);
+
+        public const int PointerMotionHintMask = (1 << 7);
+
+        public const int Button1MotionMask = (1 << 8);
+
+        public const int Button2MotionMask = (1 << 9);
+
+        public const int Button3MotionMask = (1 << 10);
+
+        public const int Button4MotionMask = (1 << 11);
+
+        public const int Button5MotionMask = (1 << 12);
+
+        public const int ButtonMotionMask = (1 << 13);
+
+        public const int KeymapStateMask = (1 << 14);
+
+        public const int ExposureMask = (1 << 15);
+
+        public const int VisibilityChangeMask = (1 << 16);
+
+        public const int StructureNotifyMask = (1 << 17);
+
+        public const int ResizeRedirectMask = (1 << 18);
+
+        public const int SubstructureNotifyMask = (1 << 19);
+
+        public const int SubstructureRedirectMask = (1 << 20);
+
+        public const int FocusChangeMask = (1 << 21);
+
+        public const int PropertyChangeMask = (1 << 22);
+
+        public const int ColormapChangeMask = (1 << 23);
+
+        public const int OwnerGrabButtonMask = (1 << 24);
+        #endregion
+
+        #region Event Name Constants
+        public const int KeyPress = 2;
+
+        public const int KeyRelease = 3;
+
+        public const int ButtonPress = 4;
+
+        public const int ButtonRelease = 5;
+
+        public const int MotionNotify = 6;
+
+        public const int EnterNotify = 7;
+
+        public const int LeaveNotify = 8;
+
+        public const int FocusIn = 9;
+
+        public const int FocusOut = 10;
+
+        public const int KeymapNotify = 11;
+
+        public const int Expose = 12;
+
+        public const int GraphicsExpose = 13;
+
+        public const int NoExpose = 14;
+
+        public const int VisibilityNotify = 15;
+
+        public const int CreateNotify = 16;
+
+        public const int DestroyNotify = 17;
+
+        public const int UnmapNotify = 18;
+
+        public const int MapNotify = 19;
+
+        public const int MapRequest = 20;
+
+        public const int ReparentNotify = 21;
+
+        public const int ConfigureNotify = 22;
+
+        public const int ConfigureRequest = 23;
+
+        public const int GravityNotify = 24;
+
+        public const int ResizeRequest = 25;
+
+        public const int CirculateNotify = 26;
+
+        public const int CirculateRequest = 27;
+
+        public const int PropertyNotify = 28;
+
+        public const int SelectionClear = 29;
+
+        public const int SelectionRequest = 30;
+
+        public const int SelectionNotify = 31;
+
+        public const int ColormapNotify = 32;
+
+        public const int ClientMessage = 33;
+
+        public const int MappingNotify = 34;
+
+        public const int GenericEvent = 35;
+
+        public const int LASTEvent = 36;
+        #endregion
+
+        #region Key Mask Constants
+        public const int ShiftMask = (1 << 0);
+
+        public const int LockMask = (1 << 1);
+
+        public const int ControlMask = (1 << 2);
+
+        public const int Mod1Mask = (1 << 3);
+
+        public const int Mod2Mask = (1 << 4);
+
+        public const int Mod3Mask = (1 << 5);
+
+        public const int Mod4Mask = (1 << 6);
+
+        public const int Mod5Mask = (1 << 7);
+        #endregion
+
+        #region Modifier Name Constants
+        public const int ShiftMapIndex = 0;
+
+        public const int LockMapIndex = 1;
+
+        public const int ControlMapIndex = 2;
+
+        public const int Mod1MapIndex = 3;
+
+        public const int Mod2MapIndex = 4;
+
+        public const int Mod3MapIndex = 5;
+
+        public const int Mod4MapIndex = 6;
+
+        public const int Mod5MapIndex = 7;
+        #endregion
+
+        #region Button Mask Constants
+        public const int Button1Mask = (1 << 8);
+
+        public const int Button2Mask = (1 << 9);
+
+        public const int Button3Mask = (1 << 10);
+
+        public const int Button4Mask = (1 << 11);
+
+        public const int Button5Mask = (1 << 12);
+
+        public const int AnyModifier = (1 << 15);
+        #endregion
+
+        #region Button Name Constants
+        public const int Button1 = 1;
+
+        public const int Button2 = 2;
+
+        public const int Button3 = 3;
+
+        public const int Button4 = 4;
+
+        public const int Button5 = 5;
+        #endregion
+
+        #region Notify Mode Constants
+        public const int NotifyNormal = 0;
+
+        public const int NotifyGrab = 1;
+
+        public const int NotifyUngrab = 2;
+
+        public const int NotifyWhileGrabbed = 3;
+
+        #endregion
+
+        #region Notify Constants
+        public const int NotifyHint = 1;
+
+        #endregion
+
+        #region Notify Detail Constants
+        public const int NotifyAncestor = 0;
+
+        public const int NotifyVirtual = 1;
+
+        public const int NotifyInferior = 2;
+
+        public const int NotifyNonlinear = 3;
+
+        public const int NotifyNonlinearVirtual = 4;
+
+        public const int NotifyPointer = 5;
+
+        public const int NotifyPointerRoot = 6;
+
+        public const int NotifyDetailNone = 7;
+
+        #endregion
+
+        #region Visibility Notify Constsants;
+
+        public const int VisibilityUnobscured = 0;
+
+        public const int VisibilityPartiallyObscured = 1;
+
+        public const int VisibilityFullyObscured = 2;
+
+        #endregion
+
+        #region Circulation Request Constants
+        public const int PlaceOnTop = 0;
+
+        public const int PlaceOnBottom = 1;
+
+        #endregion
+
+        #region Protocol Family Constants
+        public const int FamilyInternet = 0;
+
+        public const int FamilyDECnet = 1;
+
+        public const int FamilyChaos = 2;
+
+        public const int FamilyServerInterpreted = 5;
+
+        public const int FamilyInternet6 = 6;
+
+        #endregion
+
+        #region Property Notification Constants
+        public const int PropertyNewValue = 0;
+
+        public const int PropertyDelete = 1;
+
+        #endregion
+
+        #region Colormap Notification Constants
+        public const int ColormapUninstalled = 0;
+
+        public const int ColormapInstalled = 1;
+
+        #endregion
+
+        #region Grab Mode Constants
+        public const int GrabModeSync = 0;
+
+        public const int GrabModeAsync = 1;
+
+        #endregion
+
+        #region Grab Reply Status Constantsa;
+
+        public const int GrabSuccess = 0;
+
+        public const int AlreadyGrabbed = 1;
+
+        public const int GrabInvalidTime = 2;
+
+        public const int GrabNotViewable = 3;
+
+        public const int GrabFrozen = 4;
+
+        #endregion
+
+        #region AllowEvents Mode Constants
+        public const int AsyncPointer = 0;
+
+        public const int SyncPointer = 1;
+
+        public const int ReplayPointer = 2;
+
+        public const int AsyncKeyboard = 3;
+
+        public const int SyncKeyboard = 4;
+
+        public const int ReplayKeyboard = 5;
+
+        public const int AsyncBoth = 6;
+
+        public const int SyncBoth = 7;
+
+        #endregion
+
+        #region InputFocus Constants
+        public const int RevertToNone = (int)None;
+
+        public const int RevertToPointerRoot = (int)PointerRoot;
+
+        public const int RevertToParent = 2;
+
+        #endregion
+
+        #region Error Code Constants
+        public const int Success = 0;
+
+        public const int BadRequest = 1;
+
+        public const int BadValue = 2;
+
+        public const int BadWindow = 3;
+
+        public const int BadPixmap = 4;
+
+        public const int BadAtom = 5;
+
+        public const int BadCursor = 6;
+
+        public const int BadFont = 7;
+
+        public const int BadMatch = 8;
+
+        public const int BadDrawable = 9;
+
+        public const int BadAccess = 10;
+
+        public const int BadAlloc = 11;
+
+        public const int BadColor = 12;
+
+        public const int BadGC = 13;
+
+        public const int BadIDChoice = 14;
+
+        public const int BadName = 15;
+
+        public const int BadLength = 16;
+
+        public const int BadImplementation = 17;
+
+        public const int FirstExtensionError = 128;
+
+        public const int LastExtensionError = 255;
+
+        #endregion
+
+        #region Window Class Constants
+        public const int InputOutput = 1;
+
+        public const int InputOnly = 2;
+
+        #endregion
+
+        #region CreateWindow and ChangeWindowAttributes Constants
+        public const int CWBackPixmap = (1 << 0);
+
+        public const int CWBackPixel = (1 << 1);
+
+        public const int CWBorderPixmap = (1 << 2);
+
+        public const int CWBorderPixel = (1 << 3);
+
+        public const int CWBitGravity = (1 << 4);
+
+        public const int CWWinGravity = (1 << 5);
+
+        public const int CWBackingStore = (1 << 6);
+
+        public const int CWBackingPlanes = (1 << 7);
+
+        public const int CWBackingPixel = (1 << 8);
+
+        public const int CWOverrideRedirect = (1 << 9);
+
+        public const int CWSaveUnder = (1 << 10);
+
+        public const int CWEventMask = (1 << 11);
+
+        public const int CWDontPropagate = (1 << 12);
+
+        public const int CWColormap = (1 << 13);
+
+        public const int CWCursor = (1 << 14);
+
+        #endregion
+
+        #region ConfigureWindow Constants
+        public const int CWX = (1<<0);
+
+        public const int CWY = (1<<1);
+
+        public const int CWWidth = (1<<2);
+
+        public const int CWHeight = (1<<3);
+
+        public const int CWBorderWidth = (1<<4);
+
+        public const int CWSibling = (1<<5);
+
+        public const int CWStackMode = (1<<6);
+
+        #endregion
+
+        #region Bit Gravity Constants
+        public const int ForgetGravity = 0;
+
+        public const int NorthWestGravity = 1;
+
+        public const int NorthGravity = 2;
+
+        public const int NorthEastGravity = 3;
+
+        public const int WestGravity = 4;
+
+        public const int CenterGravity = 5;
+
+        public const int EastGravity = 6;
+
+        public const int SouthWestGravity = 7;
+
+        public const int SouthGravity = 8;
+
+        public const int SouthEastGravity = 9;
+
+        public const int StaticGravity = 10;
+
+        #endregion
+
+        #region Gravity Constants
+        public const int UnmapGravity = 0;
+
+        #endregion
+
+        #region Backing-Store Constants
+        public const int NotUseful = 0;
+
+        public const int WhenMapped = 1;
+
+        public const int Always = 2;
+
+        #endregion
+
+        #region Map State Constants
+        public const int IsUnmapped = 0;
+
+        public const int IsUnviewable = 1;
+
+        public const int IsViewable = 2;
+
+        #endregion
+
+        #region ChangeSaveSet Constants
+        public const int SetModeInsert = 0;
+
+        public const int SetModeDelete = 1;
+
+        #endregion
+
+        #region ChangeCloseDownMode Constants
+        public const int DestroyAll = 0;
+
+        public const int RetainPermanent = 1;
+
+        public const int RetainTemporary = 2;
+
+        #endregion
+
+        #region Window Stacking Method Constants
+        public const int Above = 0;
+
+        public const int Below = 1;
+
+        public const int TopIf = 2;
+
+        public const int BottomIf = 3;
+
+        public const int Opposite = 4;
+
+        #endregion
+
+        #region Circulation Direction Constants
+        public const int RaiseLowest = 0;
+
+        public const int LowerHighest = 1;
+
+        #endregion
+
+        #region Property Mode Constants
+        public const int PropModeReplace = 0;
+
+        public const int PropModePrepend = 1;
+
+        public const int PropModeAppend = 2;
+
+        #endregion
+
         #region Bool Constants
         /// <summary>A <see cref="int" /> value that represents <c>false</c>.</summary>
         public const int False = 0;
 
         /// <summary>A <see cref="int" /> value that represents <c>true</c>.</summary>
         public const int True = 1;
-        #endregion
-
-        #region XID Constants
-        public const int None = 0;
         #endregion
 
         #region Static Methods
@@ -682,7 +1233,7 @@ namespace TerraFX.Interop
         public static extern Status XGetWindowAttributes(
             [In] Display* display,
             [In] Window w,
-            [Out] XSetWindowAttributes* window_attributes_return
+            [Out] XWindowAttributes* window_attributes_return
         );
 
         [DllImport("libX11", BestFitMapping = false, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi, EntryPoint = "XGetWindowProperty", ExactSpelling = true, PreserveSig = true, SetLastError = false, ThrowOnUnmappableChar = false)]
