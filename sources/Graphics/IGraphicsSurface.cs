979108441cd0dc74ce66eb058666c5d65c0f50ad
Tanner Gooding <tagoo@outlook.com>
2019-09-22 08:45:25 -0700

    Marking the default interface members as public.

--- sources/Graphics/IGraphicsSurface.cs
+++ sources/Graphics/IGraphicsSurface.cs
@@ -8,23 +8,23 @@ namespace TerraFX.Graphics
     /// <summary>Represents a graphics surface.</summary>
     public interface IGraphicsSurface
     {
+        /// <summary>Gets the height of the instance.</summary>
+        public float Height => Size.Y;
+
+        /// <summary>Gets the width of the instance.</summary>
+        public float Width => Size.X;
+
         /// <summary>Gets the number of buffers for the instance.</summary>
         int BufferCount { get; }
 
         /// <summary>Gets the display handle for the instance.</summary>
         IntPtr DisplayHandle { get; }
 
-        /// <summary>Gets the height of the instance.</summary>
-        float Height => Size.Y;
-
         /// <summary>Gets the kind of surface represented by the instance.</summary>
         GraphicsSurfaceKind Kind { get; }
 
         /// <summary>Gets the size of the instance.</summary>
         Vector2 Size { get; }
-
-        /// <summary>Gets the width of the instance.</summary>
-        float Width => Size.X;
 
         /// <summary>Gets the window handle for the instance.</summary>
         IntPtr WindowHandle { get; }
