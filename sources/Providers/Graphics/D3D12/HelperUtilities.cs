583463608447fdde04ac1f6f0ef14bd25ba5ce3c
Tanner Gooding <tagoo@outlook.com>
2019-12-11 14:21:15 -0800

    Adding a ReleaseIfNotNull helper utility method

--- sources/Providers/Graphics/D3D12/HelperUtilities.cs
+++ sources/Providers/Graphics/D3D12/HelperUtilities.cs
@@ -1,13 +1,22 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.	
 
+using TerraFX.Interop;
 using static TerraFX.Interop.Windows;
 using static TerraFX.Utilities.ExceptionUtilities;
 
 namespace TerraFX.Graphics.Providers.D3D12
 {
-    internal static partial class HelperUtilities
+    internal static unsafe partial class HelperUtilities
     {
-        #region Static Methods	
+        public static void ReleaseIfNotNull<TUnknown>(TUnknown* unknown)
+            where TUnknown : unmanaged
+        {
+            if (unknown != null)
+            {
+                _ = ((IUnknown*)unknown)->Release();
+            }
+        }
+
         public static void ThrowExternalExceptionIfFailed(string methodName, int hr)
         {
             if (FAILED(hr))
@@ -15,6 +24,5 @@ namespace TerraFX.Graphics.Providers.D3D12
                 ThrowExternalException(methodName, hr);
             }
         }
-        #endregion
     }
 }
