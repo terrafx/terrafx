b98dbe7cc2e71a7d9a6f0c8f92f74592b046e0f2
Tanner Gooding <tagoo@outlook.com>
2019-12-14 08:24:14 -0800

    Updating LazyValue<T> to be disposable

--- sources/Utilities/DisposeUtilities.cs
+++ sources/Utilities/DisposeUtilities.cs
@@ -1,39 +1,13 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using static TerraFX.Utilities.AssertionUtilities;
 
 namespace TerraFX.Utilities
 {
     /// <summary>Provides a set of methods for disposing values.</summary>
     public static class DisposeUtilities
     {
-        /// <summary>Disposes of a <see cref="ValueLazy{T}" /> if <see cref="ValueLazy{T}.IsCreated" /> returns <c>true</c>.</summary>
-        /// <typeparam name="T">The type of <paramref name="value" />.</typeparam>
-        /// <param name="value">The lazy <typeparamref name="T" /> to dispose.</param>
-        public static void DisposeIfCreated<T>(ValueLazy<T> value)
-            where T : IDisposable
-        {
-            if (value.IsCreated)
-            {
-                DisposeIfNotNull(value.Value);
-            }
-        }
-
-        /// <summary>Disposes of each element in a <see cref="ValueLazy{T}" /> if <see cref="ValueLazy{T}.IsCreated" /> returns <c>true</c>.</summary>
-        /// <typeparam name="T">The type of elements in <paramref name="values" />.</typeparam>
-        /// <param name="values">The lazy <see cref="IEnumerable{T}" /> containing the values to dispose.</param>
-        public static void DisposeIfCreated<T>(ValueLazy<ImmutableArray<T>> values)
-            where T : IDisposable
-        {
-            if (values.IsCreated)
-            {
-                DisposeIfNotNull(values.Value);
-            }
-        }
-
         /// <summary>Disposes of a given <typeparamref name="T" /> if it is not <c>null</c>.</summary>
         /// <typeparam name="T">The type of <paramref name="value" />.</typeparam>
         /// <param name="value">The <typeparamref name="T" /> to dispose.</param>
@@ -46,15 +20,13 @@ namespace TerraFX.Utilities
             }
         }
 
-        /// <summary>Disposes of a each element in an <see cref="IEnumerable{T}" /> if that element is not <c>null</c>.</summary>
+        /// <summary>Disposes of a each element in an <see cref="ImmutableArray{T}" /> if that element is not <c>null</c>.</summary>
         /// <typeparam name="T">The type of elements in <paramref name="values" />.</typeparam>
-        /// <param name="values">The <see cref="IEnumerable{T}" /> containing the values to dispose.</param>
-        public static void DisposeIfNotNull<T>(IEnumerable<T> values)
+        /// <param name="values">The <see cref="ImmutableArray{T}" /> containing the values to dispose.</param>
+        public static void DisposeIfNotDefault<T>(ImmutableArray<T> values)
             where T : IDisposable
         {
-            AssertNotNull(values, nameof(values));
-
-            if (values != null)
+            if (!values.IsDefault)
             {
                 foreach (var value in values)
                 {
--- sources/Utilities/ValueLazy`1.cs
+++ sources/Utilities/ValueLazy`1.cs
@@ -5,14 +5,15 @@ using System.Runtime.InteropServices;
 using System.Threading;
 using static TerraFX.Utilities.AssertionUtilities;
 using static TerraFX.Utilities.ExceptionUtilities;
+using static TerraFX.Utilities.InteropUtilities;
+using static TerraFX.Utilities.State;
 
 namespace TerraFX.Utilities
 {
     /// <summary>Provides support for lazily initializing values.</summary>
     /// <typeparam name="T">The type of the value being lazily initialized.</typeparam>
-    public struct ValueLazy<T>
+    public struct ValueLazy<T> : IDisposable
     {
-        private const int Initialized = 1;
         private const int Creating = 2;
         private const int Created = 3;
 
@@ -25,7 +26,7 @@ namespace TerraFX.Utilities
         /// <exception cref="ArgumentNullException"><paramref name="factory" /> is <c>null</c>.</exception>
         public ValueLazy(Func<T> factory)
         {
-            this = default;
+            SkipInit(out this);
             Reset(factory);
         }
 
@@ -34,33 +35,77 @@ namespace TerraFX.Utilities
 
         /// <summary>Gets a reference to the underyling value for the instance.</summary>
         /// <remarks>This property is unsafe as it returns a reference to a struct field.</remarks>
+        /// <exception cref="ObjectDisposedException">The lazy value has been disposed.</exception>
         public ref T RefValue
         {
             get
             {
+                _state.AssertNotDisposedOrDisposing();
+
                 if (!IsCreated)
                 {
                     CreateValue();
                 }
+
                 return ref MemoryMarshal.GetReference(MemoryMarshal.CreateSpan(ref _value, 1));
             }
         }
 
         /// <summary>Gets the value for the instance.</summary>
-        public T Value => RefValue;
+        /// <exception cref="ObjectDisposedException">The lazy value has been disposed.</exception>
+        public T Value
+        {
+            get
+            {
+                _state.AssertNotDisposedOrDisposing();
+
+                if (!IsCreated)
+                {
+                    CreateValue();
+                }
+
+                return _value;
+            }
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _ = _state.BeginDispose();
+            _state.EndDispose();
+        }
+
+        /// <inheritdoc cref="Dispose()" />
+        /// <param name="action">The action to call, if the value was created, which performs the appropriate disposal.</param>
+        public void Dispose(Action<T> action)
+        {
+            var priorState = _state.BeginDispose();
+
+            if (priorState == Created)
+            {
+                action(_value);
+            }
+
+            _state.EndDispose();
+        }
 
         /// <summary>Resets the instance so the value can be recreated.</summary>
         /// <param name="factory">The factory method to call when initializing the value.</param>
         /// <exception cref="ArgumentNullException"><paramref name="factory" /> is <c>null</c>.</exception>
+        /// <exception cref="ObjectDisposedException">The lazy value has been disposed.</exception>
         public void Reset(Func<T> factory)
         {
+            _state.ThrowIfDisposedOrDisposing();
             ThrowIfNull(factory, nameof(factory));
+
             _factory = factory;
             _ = _state.Transition(to: Initialized);
         }
 
         private void CreateValue()
         {
+            _state.ThrowIfDisposedOrDisposing();
+
             var spinWait = new SpinWait();
 
             while (!IsCreated)
