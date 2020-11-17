1968cc20931e265b6c5f0499c1fb28d2789e2d6e
Ingmar Bitter <ingmar.bitter@gmail.com>
2020-11-17 13:16:12 -0500

    HelloSierpinski reduced to just geometry and rotation transformation. Works fine

--- samples/TerraFX/Graphics/HelloSierpinski.cs
+++ samples/TerraFX/Graphics/HelloSierpinski.cs
@@ -35,17 +35,17 @@ namespace TerraFX.Samples.Graphics
             base.Initialize(application);
 
             var graphicsDevice = GraphicsDevice;
+
             ulong vertices = 2 * 12 * (ulong)MathF.Pow(4, _recursionDepth);
             ulong vertexBufferSize = vertices * SizeOf<PosNormTex3DVertex>();
             ulong indexBufferSize = vertices * SizeOf<uint>(); // matches vertices count because vertices are replicated, three unique ones per triangle
-            using (var vertexStagingBuffer = graphicsDevice.MemoryAllocator.CreateBuffer(GraphicsBufferKind.Default, GraphicsResourceCpuAccess.Write, vertexBufferSize)) 
+            using (var vertexStagingBuffer = graphicsDevice.MemoryAllocator.CreateBuffer(GraphicsBufferKind.Default, GraphicsResourceCpuAccess.Write, vertexBufferSize))
             using (var indexStagingBuffer = graphicsDevice.MemoryAllocator.CreateBuffer(GraphicsBufferKind.Default, GraphicsResourceCpuAccess.Write, indexBufferSize))
-            using (var textureStagingBuffer = graphicsDevice.MemoryAllocator.CreateBuffer(GraphicsBufferKind.Default, GraphicsResourceCpuAccess.Write, 64 * 1024 * 1024)) // 2^26, same as 4 * 256 * 256 * 256
             {
                 var currentGraphicsContext = graphicsDevice.CurrentContext;
 
                 currentGraphicsContext.BeginFrame();
-                _pyramid = CreateGraphicsPrimitive(currentGraphicsContext, vertexStagingBuffer, indexStagingBuffer, textureStagingBuffer);
+                _pyramid = CreateGraphicsPrimitive(currentGraphicsContext, vertexStagingBuffer, indexStagingBuffer);
                 currentGraphicsContext.EndFrame();
 
                 graphicsDevice.Signal(currentGraphicsContext.Fence);
@@ -71,7 +71,7 @@ namespace TerraFX.Samples.Graphics
 
             // Shaders take transposed matrices, so we want to mirror along the diagonal
 
-            bool isIdentity = true; if (isIdentity) pConstantBuffer[0] = new Matrix4x4(
+            bool isIdentity = false; if (isIdentity) pConstantBuffer[0] = new Matrix4x4(
                 new Vector4(1.0f, 0.0f, 0.0f, 0.0f),
                 new Vector4(0.0f, 1.0f, 0.0f, 0.0f),
                 new Vector4(0.0f, 0.0f, 1.0f, 0.0f),
@@ -92,7 +92,7 @@ namespace TerraFX.Samples.Graphics
                 new Vector4(0.0f, 0.0f, 0.0f, 1.0f)
             );
 
-            bool isRotateAroundZ = false; if (isRotateAroundZ) pConstantBuffer[0] = new Matrix4x4(
+            bool isRotateAroundZ = true; if (isRotateAroundZ) pConstantBuffer[0] = new Matrix4x4(
                 new Vector4(+cos, -sin, 0.0f, 0.0f),
                 new Vector4(+sin, +cos, 0.0f, 0.0f),
                 new Vector4(0.0f, 0.0f, 1.0f, 0.0f),
@@ -108,42 +108,37 @@ namespace TerraFX.Samples.Graphics
             base.Draw(graphicsContext);
         }
 
-        private unsafe GraphicsPrimitive CreateGraphicsPrimitive(GraphicsContext graphicsContext, GraphicsBuffer vertexStagingBuffer, GraphicsBuffer indexStagingBuffer, GraphicsBuffer textureStagingBuffer)
+        private unsafe GraphicsPrimitive CreateGraphicsPrimitive(GraphicsContext graphicsContext, GraphicsBuffer vertexStagingBuffer, GraphicsBuffer indexStagingBuffer)
         {
             var graphicsDevice = GraphicsDevice;
             var graphicsSurface = graphicsDevice.Surface;
 
-            var graphicsPipeline = CreateGraphicsPipeline(graphicsDevice, "Sierpinski", "main", "main");
+            var graphicsPipeline = CreateGraphicsPipeline(graphicsDevice, "Transform", "main", "main");
             (List<Vector3> vertices, List<uint> indices) = SierpinskiPyramid.CreateMeshQuad(_recursionDepth);
             List<Vector3> normals = SierpinskiPyramid.MeshNormals(vertices);
-            var vertexBuffer = CreateVertexBuffer(vertices, normals, graphicsContext, vertexStagingBuffer);
+
+            var vertexBuffer = CreateVertexBuffer(vertices, normals, graphicsContext, vertexStagingBuffer, aspectRatio: graphicsSurface.Width / graphicsSurface.Height);
             var indexBuffer = CreateIndexBuffer(indices, graphicsContext, indexStagingBuffer);
 
-            var inputResources = new GraphicsResource[3] {
+            var inputResources = new GraphicsResource[2] {
                 CreateConstantBuffer(graphicsContext),
                 CreateConstantBuffer(graphicsContext),
-                CreateTexture3D(graphicsContext, textureStagingBuffer),
+                //CreateTexture3D(graphicsContext, textureStagingBuffer),
             };
-            return graphicsDevice.CreatePrimitive(graphicsPipeline, new GraphicsBufferView(vertexBuffer, vertexBuffer.Size, SizeOf<PosNormTex3DVertex>()), new GraphicsBufferView(indexBuffer, indexBuffer.Size, sizeof(ushort)), inputResources);
+            return graphicsDevice.CreatePrimitive(graphicsPipeline, new GraphicsBufferView(vertexBuffer, vertexBuffer.Size, SizeOf<IdentityVertex>()), new GraphicsBufferView(indexBuffer, indexBuffer.Size, sizeof(uint)), inputResources);
 
-            static GraphicsBuffer CreateVertexBuffer(List<Vector3> vertices, List<Vector3> normals, GraphicsContext graphicsContext, GraphicsBuffer vertexStagingBuffer)
+            static GraphicsBuffer CreateVertexBuffer(List<Vector3> vertices, List<Vector3> normals, GraphicsContext graphicsContext, GraphicsBuffer vertexStagingBuffer, float aspectRatio)
             {
-                int size = sizeof(PosNormTex3DVertex) * vertices.Count;
+                int size = sizeof(IdentityVertex) * vertices.Count;
                 var vertexBuffer = graphicsContext.Device.MemoryAllocator.CreateBuffer(GraphicsBufferKind.Vertex, GraphicsResourceCpuAccess.None, (ulong)size);
-                var pVertexBuffer = vertexStagingBuffer.Map<PosNormTex3DVertex>();
+                var pVertexBuffer = vertexStagingBuffer.Map<IdentityVertex>();
 
-                // assumes the vertices are in a box from (-1,-1,-1) to (1,1,1)
-                var offset3D = new Vector3(1, 1, 1); // to move lower left corner to (0,0,0)
-                var scale3D = new Vector3(0.5f, 0.5f, 0.5f); // to scale to side length 1
                 for (int i = 0; i < vertices.Count; i++)
                 {
                     var xyz = vertices[i];                // position
-                    var normal = normals[i];              // normal
-                    var uvw = (xyz + offset3D) * scale3D; // texture coordinate
-                    pVertexBuffer[i] = new PosNormTex3DVertex {
+                    pVertexBuffer[i] = new IdentityVertex {
                         Position = xyz,
-                        Normal = normal,
-                        UVW = uvw
+                        Color = new Vector4(xyz.X, xyz.Y, 0.5f, 1.0f),
                     };
                 }
 
@@ -181,32 +176,6 @@ namespace TerraFX.Samples.Graphics
                 return constantBuffer;
             }
 
-            static GraphicsTexture CreateTexture3D(GraphicsContext graphicsContext, GraphicsBuffer textureStagingBuffer)
-            {
-                const uint TextureWidth = 256;
-                const uint TextureHeight = 256;
-                const ushort TextureDepth = 256;
-                const uint TextureDz = TextureWidth * TextureHeight;
-                const uint TexturePixels = TextureDz * TextureDepth;
-                const uint TextureSize = TexturePixels * 4;
-
-                var texture3D = graphicsContext.Device.MemoryAllocator.CreateTexture(GraphicsTextureKind.ThreeDimensional, GraphicsResourceCpuAccess.None
-                    , TextureWidth, TextureHeight, TextureDepth);
-                var pTextureData = textureStagingBuffer.Map<uint>();
-
-                for (uint n = 0; n < TexturePixels; n++)
-                {
-                    var x = n % TextureWidth;
-                    var y = (n % TextureDz) / TextureWidth;
-                    var z = n / TextureDz;
-
-                    pTextureData[n] = 0xFF000000 | (z << 16) | (y << 8) | (x << 0);
-                }
-                textureStagingBuffer.Unmap(0..(int)TextureSize);
-                graphicsContext.Copy(texture3D, textureStagingBuffer);
-
-                return texture3D;
-            }
 
             GraphicsPipeline CreateGraphicsPipeline(GraphicsDevice graphicsDevice, string shaderName, string vertexShaderEntryPoint, string pixelShaderEntryPoint)
             {
@@ -217,22 +186,21 @@ namespace TerraFX.Samples.Graphics
                 return graphicsDevice.CreatePipeline(signature, vertexShader, pixelShader);
             }
 
-            static GraphicsPipelineSignature CreateGraphicsPipelineSignature(GraphicsDevice graphicsDevice)
+            GraphicsPipelineSignature CreateGraphicsPipelineSignature(GraphicsDevice graphicsDevice)
             {
                 var inputs = new GraphicsPipelineInput[1] {
                     new GraphicsPipelineInput(
-                        new GraphicsPipelineInputElement[3] {
+                        new GraphicsPipelineInputElement[2] {
                             new GraphicsPipelineInputElement(typeof(Vector3), GraphicsPipelineInputElementKind.Position, size: 12),
-                            new GraphicsPipelineInputElement(typeof(Vector3), GraphicsPipelineInputElementKind.Normal, size: 12),
-                            new GraphicsPipelineInputElement(typeof(Vector3), GraphicsPipelineInputElementKind.TextureCoordinate, size: 12),
+                            new GraphicsPipelineInputElement(typeof(Vector4), GraphicsPipelineInputElementKind.Color, size: 16),
                         }
                     ),
                 };
 
-                var resources = new GraphicsPipelineResource[3] {
+                var resources = new GraphicsPipelineResource[2] {
                     new GraphicsPipelineResource(GraphicsPipelineResourceKind.ConstantBuffer, GraphicsShaderVisibility.Vertex),
                     new GraphicsPipelineResource(GraphicsPipelineResourceKind.ConstantBuffer, GraphicsShaderVisibility.Vertex),
-                    new GraphicsPipelineResource(GraphicsPipelineResourceKind.Texture, GraphicsShaderVisibility.Pixel),
+                    //new GraphicsPipelineResource(GraphicsPipelineResourceKind.Texture, GraphicsShaderVisibility.Pixel),
                 };
 
                 return graphicsDevice.CreatePipelineSignature(inputs, resources);
