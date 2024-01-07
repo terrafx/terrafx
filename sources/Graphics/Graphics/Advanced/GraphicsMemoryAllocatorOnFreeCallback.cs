5fe8935871af00c892f1014f26d4897177d053aa
Tanner Gooding <tagoo@outlook.com>
2024-01-07 11:21:31 -0800

    Use primary constructors where possible

--- sources/Graphics/Graphics/Advanced/GraphicsMemoryAllocatorOnFreeCallback.cs
+++ sources/Graphics/Graphics/Advanced/GraphicsMemoryAllocatorOnFreeCallback.cs
@@ -7,17 +7,12 @@ using System.Runtime.InteropServices;
 namespace TerraFX.Graphics.Advanced;
 
 /// <summary>Defines a function which is raised when a graphics memory region is freed.</summary>
+/// <remarks>Initializes a new instance of the <see cref="GraphicsMemoryAllocatorOnFreeCallback" /> struct.</remarks>
+/// <param name="value">A pointer to the function that will be called in <see cref="Invoke" />.</param>
 [StructLayout(LayoutKind.Auto)]
-public readonly unsafe struct GraphicsMemoryAllocatorOnFreeCallback
+public readonly unsafe struct GraphicsMemoryAllocatorOnFreeCallback(delegate*<in GraphicsMemoryRegion, void> value)
 {
-    private readonly delegate*<in GraphicsMemoryRegion, void> _value;
-
-    /// <summary>Initializes a new instance of the <see cref="GraphicsMemoryAllocatorOnFreeCallback" /> struct.</summary>
-    /// <param name="value">A pointer to the function that will be called in <see cref="Invoke" />.</param>
-    public GraphicsMemoryAllocatorOnFreeCallback(delegate*<in GraphicsMemoryRegion, void> value)
-    {
-        _value = value;
-    }
+    private readonly delegate*<in GraphicsMemoryRegion, void> _value = value;
 
     /// <summary>Gets <c>true</c> if the backing function pointer is not null; otherwise, <c>false</c>.</summary>
     public bool IsNotNull => _value is not null;
